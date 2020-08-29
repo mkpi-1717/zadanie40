a3b8c2347f428e68c5f749da2c5df39f860d56cc
mkpi-1717 <marekpisarczyk22@gmail.com>
2020-08-29 20:45:53 +0200

    wykonano zadanie40

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+function formSubmit(){
+    let name = document.getElementById('namef').value;
+    let surname = document.getElementById('surnamef').value;
+    let phone = document.getElementById('phonef').value;
+
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("surname").innerHTML = surname;
+    document.getElementById("phone").innerHTML = phone;
+}
+
