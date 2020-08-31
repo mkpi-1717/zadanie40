a082a98a57ed8926973a544ae8cf93c90332a1db
mkpi-1717 <marekpisarczyk22@gmail.com>
2020-08-31 18:10:21 +0200

    korekta

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 
 
-myButton.onclick = function myF(){
+
+ function myFunction () {
     let name = document.getElementById('namef').value;
     let surname = document.getElementById('surnamef').value;
     let phone = document.getElementById('phonef').value;
 
-    document.getElementById("name").innerHTML = name;
-    document.getElementById("surname").innerHTML = surname;
-    document.getElementById("phone").innerHTML = phone;
+    document.getElementById('name').innerHTML = name;
+    document.getElementById('surname').innerHTML = surname;
+    document.getElementById('phone').innerHTML = phone;
 }
 
+document.getElementById('myButton').onclick = myFunction;
